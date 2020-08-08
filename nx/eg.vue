692c6a264681c2d1ada3f7dd9fab13389e682834
lioegrz4 <dujun2001624@126.com>
2020-08-08 16:22:10 +0800

    新文件：   nx/eg.vue 	删除：     nx/example.js 	删除：     nx/test.vue

---
+++ nx/eg.vue
@@ -0,0 +1,55 @@
+<template>
+    <div style="display: flex">
+        <input type="checkbox" v-model="NxEditable"/>
+        <Nx :payload="NxValue" :editable="NxEditable"></Nx>
+        <NxDock v-if="NxEditable" style="padding: 0.5em;"></NxDock>
+    </div>
+</template>
+<script>
+import Nx from "./index.vue";
+import NxDock from "./dock.vue";
+
+export default {
+  name: "nxText",
+  components: { Nx, NxDock },
+  data() {
+    return {
+      NxValue: NxData,
+      NxEditable: true
+    };
+  }
+};
+
+const NxData = {
+  type: "container",
+  props: {
+    x: 123
+  },
+  children: [{
+      type: "container",
+      props: {
+        y: 456
+      },
+      children: [{
+        type: "logo",
+        props: {
+          value: 789
+        }
+      }]
+    },
+    {
+      type: "container",
+      props: {
+        z: 111
+      },
+      children: [{
+        type: "logo",
+        props: {
+          value: 111
+        }
+      }]
+    }
+  ]
+}
+
+</script>
