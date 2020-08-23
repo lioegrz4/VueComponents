f49172734c30b8453cf9dc3c2cce48f83a4b5f32
lioegrz4 <dujun2001624@126.com>
2020-08-23 16:28:36 +0800

    新文件：   flow/index.vue

---
+++ flow/index.vue
@@ -0,0 +1,126 @@
+<script>
+import _ from "lodash/fp";
+let L = _.noConflict();
+
+export default {
+  name: "FlowContainer",
+  props: {
+    doneText: {
+      default: "完成"
+    },
+    nextText: {
+      default: "下一步"
+    },
+    submitMethod: {
+      default: "submitUp"
+    }
+  },
+  data() {
+    let items = this.$slots.default
+        .map( (x, ix) => x.tag
+              ? {
+                  label: L.get(["data", "attrs", "label"])(x),
+                  done: false,
+                  ready: false,
+                  ix,
+                  ...L.get(["data", "attrs"])(x)
+                }
+              : undefined
+            )
+        .filter(L.identity)
+    return {
+      items,
+      currentTab: null,
+      nextTab: null,
+    };
+  },
+  computed: {
+    todo() {
+      return _.find(x => !x.done)(this.items) || {};
+    },
+    firstUnReady() {
+      return _.find(x => !x.ready)(this.items) || {};
+    },
+    end() {
+      return _.last(this.items)
+    },
+    current() {
+      let ix = _.findIndex({label: this.currentTab || this.todo.label})(this.items)
+      this.nextTab = this.items[ix+1] && this.items[ix+1].label
+      return this.items[ix]
+    }
+  },
+  methods: {
+    onStateChange(ix, prop, v = true) {
+      if (!v) this.currentTab = null
+      _.find({ix})(this.items)[prop] = v
+    },
+    async onNext() {
+      this.onStateChange(this.current.ix, "done");
+      if (this.end.ix === this.current.ix) {
+        let ref = this.$refs[this.end.ix].$children[0]
+        let fn = ref[this.submitMethod].bind(ref)
+        this.$emit('done', await fn())
+      } else {
+        this.currentTab = this.nextTab
+      }
+    },
+    onTabClick(v) {
+      this.currentTab = v
+    }
+  },
+  render(h) {
+    let self = this;
+    return (
+      <el-tabs type="border-card" class="flow-container"
+               activeName={this.current.label}
+               on-input={this.onTabClick} >
+        {this.items.map(x => {
+          let n = this.$slots.default[x.ix];
+          n.componentOptions.ref = x.ix
+          // 在子组件上监听事件
+          if (!n.componentOptions) return;
+          n.componentOptions.listeners = _.assign(
+            n.componentOptions.listeners,
+            {
+              done(v) {
+                self.onStateChange(x.ix, "ready", v);
+                self.onStateChange(x.ix, "done", v);
+              },
+              ready(v) {
+                self.onStateChange(x.ix, "ready", v);
+              }
+            }
+          );
+          return (
+            <el-tab-pane key={x.ix} disabled={x.ix > this.firstUnReady.ix}
+                         label={x.label} name={x.label} ref={x.ix}>
+              {n}
+            </el-tab-pane>
+          );
+        })}
+        <div class="flow-action_bar">
+          <el-button disabled={!this.current.ready}
+                     on-click={this.onNext} >
+             { this.end.label === this.currentTab ? this.doneText : this.nextText }
+          </el-button>
+        </div>
+      </el-tabs>
+    );
+  }
+};
+</script>
+<style>
+.flow-container .el-tabs__content {
+  position: relative;
+}
+.flow-container .flow-action_bar {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+}
+.el-tabs__item.is-disabled {
+  color: rgb(222, 222, 222) !important;
+}
+</style>
