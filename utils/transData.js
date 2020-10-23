1f3695be112f627f89caa1b106e7725c7079974b
lioegrz4 <dujun2001624@126.com>
2020-10-23 20:48:59 +0800

    transData

--- utils/transData.js
+++ utils/transData.js
@@ -1,56 +1,95 @@
-const { getIn } = require('./lambda')
-const { objWalker } = require('./tree')
-/*
-不定长列表有两种匹配模式：有限模式和无限模式
- - 有限模式 [a, b, c] =:= [1,2,3,4...]
- - 无限模式 a =!= [1,2,3...]
-          {a:'x', b:'y'} = [{a:1, b:2}, {a:1, b:2}, {a:1, b:2}...]
-   使用非列表匹配列表时，使用无限模式
- - 路径 [a,b, c]
-解析模式时，遇到列表声明中止解析，存储后续模式。匹配时，遇到列表递归调用解析函数
- - 如何安全的声明列表
-   - 靠谱的方法
- - 不中止解析，如果在解析结果中找到列表的定义
-   - smartGetIn ，遇到列表，返回当前路径。主调方收到后，回退到列表处重新解析
-     - 返回和类型 Result，分多种情况
-       - 获取到值
-       - 遇到列表
-       - 没获取到值
-     - 重新解析后，用解析到的模式 Map 列表
-   - 遇到列表时可知，然而无限模式的其它声明可能散落在多个模式定义中
-     - objWalker 无法预知遇到的结构是列表还是字典
-   - 使用某种数据结构解决这个问题
- */
-const transData = (pattern, target)  => value => {
-  // 解析模式独立出来
-  let varMap = {}
-  objWalker(pattern, (k, v, p) => {
-    if (v) {
-      varMap[v] = p.concat([k])
+const isArray = x => x instanceof Array
+const isObject = x => typeof x === 'object' && !(x instanceof Array)
+const parsePath = x => x.split('.')
+const get = (s, path) => path.reduce((curr, x) => curr && curr[x], s)
+
+let mock = {
+    a: 123,
+    b: 123,
+    c: {
+        d: [{
+                e: 1,
+                f: [{x: [{y: 1}]}],
+                g: {
+                    a: 1
+                }
+            },
+            {
+                e: 2,
+                f: [{x: [{y: 2}]}],
+                g: {
+                    a: 5
+                }
+            }
+        ]
     }
-    return v
-  })
-  // 匹配模式拆分开来
-  if (target){
-    objWalker(target, (k, v, p) => {
-      if (isNaN(parseInt(k))) {
-        let r = getIn(value, varMap[k])
-        return r
-      } else {
-        // 如果键是数字字符串，说明在列表中且没有键，则把当前值作为键
-        let realK = getIn(target, p.concat([k]))
-        let r = getIn(value, varMap[realK])
-        return r
-      }
-    })
-    return target
-  } else {
-    let r = {}
-    Object.keys(varMap).forEach(x => r[x] = getIn(value, varMap[x]))
-    return r
-  }
 }
 
+let schema = {
+    a: 'b',
+    c: {
+        d: {
+            f: {
+                x: {
+                    y: 'z'
+                }
+            },
+            g: {
+                a: "b.c"
+            }
+        }
+    }
+}
+
+
+const mergeArray = ([first, ...rest]) => {
+    for (let i in first) {
+        first[i] = [first[i]]
+    }
+    for (let i of rest) {
+        for (let j in first) {
+            first[j].push(i[j])
+        }
+    }
+    return first
+}
+const transData = (schema, src) => {
+    if (isArray(src)) {
+        return src.map(x => {
+            let r = transData(schema, x)
+            return r
+            /*
+            if (isArray(r) && r.length===1) {
+                return r[0]
+            } else {
+                return r
+            }
+            */
+        })
+    } else {
+        let ret = {}
+        for (let i in schema) {
+            if (isObject(schema[i])) {
+                let r = transData(schema[i], src[i])
+                if (isArray(r)) {
+                    if (r.length === 1) {
+                        Object.assign(ret, r[0])
+                    } else {
+                        Object.assign(ret, {[i]: r})
+                    }
+                } else {
+                    Object.assign(ret,  r)
+                }
+            } else {
+                ret[schema[i]] = src[i]
+            }
+        }
+        return ret
+    }
+}
+
+//let r = transData(schema, mock)
+
 module.exports = {
-  transData
+    transData
 }
