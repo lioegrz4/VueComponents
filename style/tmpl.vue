3a8d37b4f960f14a9a43ece7cad7f1e8fe252dc8
lioegrz4 <dujun2001624@126.com>
2020-11-25 10:03:21 +0800

    tmpl

---
+++ style/tmpl.vue
@@ -0,0 +1,21 @@
+<style lang="stylus" scoped>
+
+</style>
+<template lang="pug">
+
+</template>
+<script lang="ts">
+import { Vue, Component, Prop, Emit, Watch } from "vue-property-decorator"
+import { namespace, Getter, Mutation, Action }    from 'vuex-class'
+import _                        from "lodash/fp"
+
+@Component({
+    name: "",
+})
+export default class extends Vue {
+    @Prop() value
+    @Emit() input(v) {
+        return v
+    }
+}
+</script>
