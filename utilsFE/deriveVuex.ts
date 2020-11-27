d520c8f72767455dbebfce5c2fe8c062ce7f87b7
lioegrz4 <dujun2001624@126.com>
2020-11-27 15:58:15 +0800

    vuex driving setterM

--- utilsFE/deriveVuex.ts
+++ utilsFE/deriveVuex.ts
@@ -1,4 +1,21 @@
 import { merge, upperFirst } from 'lodash/fp'
+import Vue from 'vue'
+import {isArray, isPlainObject} from 'lodash/fp'
+
+const mkGetter  = i => state => state[i]
+const mkSetter  = i => (state, payload) => state[i] = payload
+const mkSetterA = i => (state, payload) => state[i].push(payload)
+const mkSetterM = i => (state, payload) => {
+    if (!isArray(payload) || payload.length!==2) throw `setter#${i}:payload require [key, value]`
+    Vue.set(state[i], payload[0], payload[1])
+}
+
+const getSetter = (obj, i) => {
+    if (isArray(obj)) return mkSetterA(i)
+    if (isPlainObject(obj)) return mkSetterM(i)
+    return mkSetter(i)
+}
+
 
 export function deriveModule (dfts, config) {
     let fields = Object.keys(dfts)
@@ -11,8 +28,7 @@ export function deriveModule (dfts, config) {
     }
     for (let i of fields) {
         r.state[i] = dfts[i]
-        let getter = state => state[i]
-          , setter = (state, payload) => state[i] = payload
+        let getter = mkGetter(i), setter = getSetter(dfts[i],i)
         r.getters['get' + upperFirst(i)] = getter
         r.getters[i] = getter
         r.mutations['set' + upperFirst(i)] = setter
