ad9ff1095e05afa84d4c3dfed21363ca25223296
lioegrz4 <dujun2001624@126.com>
2020-12-04 23:05:32 +0800

    webpackImportCtx

--- utilsFE/importComponent.js
+++ utilsFE/importComponent.js
@@ -1,17 +1,10 @@
-import Vue from 'vue'
+import { importComponents, importVuex, importRouter } from './webpackImportCtx'
 
-function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1)
-}
+importComponents(require.context('../widgets', true, /index\.vue$/))
+importComponents(require.context('../components', true, /index\.vue$/))
 
-const importDir = (requireComponent, handleFileName) => {
-  let hfn = handleFileName || (x => x)
-  requireComponent.keys().forEach(fileName => {
-    const componentConfig = requireComponent(fileName)
-    const componentName = fileName.replace(/\.\/(.+)\/index.vue$/g, "$1")
-    Vue.component(hfn(componentName), componentConfig.default || componentConfig)
-  })
-}
+export const store = importVuex(require.context('../store', true, /.+\.js/))
 
-importDir(require.context( '../widgets', true, /index\.vue$/ ))
-importDir(require.context( '../components', true, /index\.vue$/ ))
+store.dispatch('ws/listen')
+
+// export const router = importRouter({}, require.context('../pages', true, /(index\.vue|info\.yml)$/))
