2f991f506c8019bd633d14a9faa6c58ef7576429
lioegrz4 <dujun2001624@126.com>
2020-12-09 16:39:34 +0800

    dsi

--- utilsFE/driveSocketIO.js
+++ utilsFE/driveSocketIO.js
@@ -4,8 +4,8 @@ global.SOCKETIO_LISTENER = socketioListeners
 export const deriveSocketIO = {
     type: 'socket.io',
     handler({setter, getter, client}, prop) {
-        let init = ({rootGetters}) => {
-            rootGetters['socketio/client'].on(prop, getter)
+        let init = (ctx) => {
+            ctx.rootGetters['socketio/client'].on(prop, x => setter(ctx, x))
         }
         let action = async(ctx, ...args) => {
             await setter(ctx, await getter(ctx, ...args))
