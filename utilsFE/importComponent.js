13a01363bc7ccfb3c0384d2fd29268e3c3d33538
lioegrz4 <dujun2001624@126.com>
2020-12-09 15:42:07 +0800

    ..

--- utilsFE/importComponent.js
+++ utilsFE/importComponent.js
@@ -3,8 +3,6 @@ import { importComponents, importVuex, importRouter } from './webpackImportCtx'
 importComponents(require.context('../widgets', true, /index\.vue$/))
 importComponents(require.context('../components', true, /index\.vue$/))
 
-export const store = importVuex(require.context('../store', true, /.+\.js/))
-
-store.dispatch('ws/listen')
+export const { store, init } = importVuex(require.context('../store', true, /.+\.js/))
 
 // export const router = importRouter({}, require.context('../pages', true, /(index\.vue|info\.yml)$/))
--- utilsFE/webpackImportCtx.js
+++ utilsFE/webpackImportCtx.js
@@ -1,6 +1,7 @@
 export const importCtx = handler => ctx => {
     return ctx.keys().map(handler(ctx))
 }
+import { get } from 'lodash/fp'
 
 import Vue from 'vue'
 export const vueImportHanlder = requireComponent => fileName => {
@@ -13,13 +14,17 @@ export const importComponents = importCtx(vueImportHanlder)
 import Vuex from 'vuex'
 Vue.use(Vuex)
 export const importVuex = requireComponent => {
-    let modules = {}
+    let modules = {}, init = []
     requireComponent.keys().forEach(fileName => {
         const m = requireComponent(fileName)
         const n = fileName.replace(/\.\/(.+)\.js$/g, "$1")
         modules[n] = m.default || m
+        if (get([n, 'actions', '__init__'])(modules)) {
+            init.push(`${n}/__init__`)
+        }
     })
-    return new Vuex.Store({ modules })
+    let store = new Vuex.Store({ modules })
+    return { store, init }
 }
 
 import VueRouter from 'vue-router'
