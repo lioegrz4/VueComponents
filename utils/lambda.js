3838ecfd738df68c9876443db1c0ce49c4fee3bf
lioegrz4 <dujun2001624@126.com>
2021-02-18 14:24:31 +0800

    logF

--- utils/lambda.js
+++ utils/lambda.js
@@ -240,6 +240,11 @@ const wrapCache    = (cache, kfmt) => async ({key, get}) => {
   return r['payload']
 }
 
+const logF         = ({fs, path, sep}) => (...content) => {
+  let p = content.length===1 && content[0] instanceof Array ? content[0] : content
+  fs.appendFileSync(path, [ new Date().toISOString(), ...p ].join(sep || '\t') + '\n' )
+}
+
 module.exports = { p
                  , flip
                  , id
@@ -286,4 +291,5 @@ module.exports = { p
                  , muteGet
                  , objToKey
                  , wrapCache
+                 , logF
                  }
