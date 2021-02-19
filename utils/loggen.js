9153e1361683994a529b382a0db8812765b8e2cc
lioegrz4 <dujun2001624@126.com>
2021-02-19 12:26:26 +0800

    rfc

--- utils/loggen.js
+++ utils/loggen.js
@@ -2,16 +2,12 @@ const defaultLvProbe = (d='error') => () => process.env.NODE_LOG || d
 
 const eq = v => x => x===v
 
-const fsHandler = ({ fs, path, sep = "\t", tagSep = ":", lvProbe }) => async (ls, level, tag, content) => {
-    let Ev = ls.findIndex(eq(lvProbe())), Lv = ls.findIndex(eq(level))
-    if (Lv < Ev) return
+const fsHandler = ({ fs, path, sep = "\t", tagSep = ":", lvProbe }) => async (level, tag, content) => {
     await fs.appendFileSync(path, [new Date().toISOString(), level, tag.join(tagSep), ...content].join(sep) + '\n')
 }
 
 const csHandler = typeof window === 'object'
-    ? (title, lvProbe, lvFmt) => (ls, level, tag, content) => {
-        let Ev = ls.findIndex(eq(lvProbe())), Lv = ls.findIndex(eq(level))
-        if (Lv < Ev) return
+    ? (title, lvProbe, lvFmt) => (level, tag, content) => {
         let LEAD = "color:MediumSlateBlue;font-weight:bold"
         let TAG = "color:Silver"
         if (tag.length > 0) {
@@ -27,9 +23,7 @@ const csHandler = typeof window === 'object'
                 , ...content)
         }
     }
-    : (title, lvProbe, lvFmt) => (ls, level, tag, content) => {
-        let Ev = ls.findIndex(eq(lvProbe())), Lv = ls.findIndex(eq(level))
-        if (Lv < Ev) return
+    : (title, lvProbe, lvFmt) => (level, tag, content) => {
         if (tag.length > 0) {
             console.log(`<--- ${title} <= ${tag.join(' <= ')}\n`
                 , ...content)
@@ -39,17 +33,23 @@ const csHandler = typeof window === 'object'
         }
     }
 
-const loggen = ({ level = ['trace', 'debug', 'info', 'warn', 'error', 'fatal'], handler }) => {
+const loggen = ({ level = ['trace', 'debug', 'info', 'warn', 'error', 'fatal'], handler, probe }) => {
     function logger() { }
-    let genMethod = (ls, level, handler) => (tag, ...content) => handler(ls, level, tag, content)
+    let genMethod = (ls, level, handler) => {
+        let Ev = ls.findIndex(eq(probe())), Lv = ls.findIndex(eq(level))
+        return (tag, ...content) => {
+            if (Lv < Ev) return
+            handler(level, tag, content)
+        }
+    }
     for (let l of level) {
         logger.prototype[l] = genMethod(level, l, handler)
     }
     return new logger()
 }
 
-const conslog = title => loggen({ handler: csHandler(title, defaultLvProbe()) })
-const filelog = (fs, path) => loggen({ handler: fsHandler({fs, path, lvProbe: defaultLvProbe('info')})})
+const conslog = title => loggen({ handler: csHandler(title), probe: defaultLvProbe() })
+const filelog = (fs, path) => loggen({ handler: fsHandler({fs, path}), probe: defaultLvProbe('info')})
 
 module.exports = {
     loggen, fsHandler, csHandler, conslog, filelog
@@ -62,4 +62,3 @@ l1 = filelog(fs, './log')
 l1.debug([], 123)
 l1.info(['a','b','c'], 23)
 */
-
