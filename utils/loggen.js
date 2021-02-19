6e722354ee8fcc9715ea479f34a83f69e99a1742
lioegrz4 <dujun2001624@126.com>
2021-02-19 12:06:52 +0800

    loggen level

--- utils/loggen.js
+++ utils/loggen.js
@@ -1,12 +1,17 @@
-const REV = typeof window === 'object' ? 'B' : 'N'
+const defaultLvProbe = (d='error') => () => process.env.NODE_LOG || d
 
-const fsHandler = ({ fs, path, sep = "\t", tagSep = ":" }) => (level, tag, content) => {
+const eq = v => x => x===v
+
+const fsHandler = ({ fs, path, sep = "\t", tagSep = ":", lvProbe }) => (ls, level, tag, content) => {
+    let Ev = ls.findIndex(eq(lvProbe())), Lv = ls.findIndex(eq(level))
+    if (Lv < Ev) return
     fs.appendFileSync(path, [new Date().toISOString(), level, tag.join(tagSep), ...content].join(sep) + '\n')
 }
 
-
 const csHandler = typeof window === 'object'
-    ? (title, lvFmt) => (level, tag, content) => {
+    ? (title, lvProbe, lvFmt) => (ls, level, tag, content) => {
+        let Ev = ls.findIndex(eq(lvProbe())), Lv = ls.findIndex(eq(level))
+        if (Lv < Ev) return
         let LEAD = "color:MediumSlateBlue;font-weight:bold"
         let TAG = "color:Silver"
         if (tag.length > 0) {
@@ -22,7 +27,9 @@ const csHandler = typeof window === 'object'
                 , ...content)
         }
     }
-    : (title, lvFmt) => (level, tag, content) => {
+    : (title, lvProbe, lvFmt) => (ls, level, tag, content) => {
+        let Ev = ls.findIndex(eq(lvProbe())), Lv = ls.findIndex(eq(level))
+        if (Lv < Ev) return
         if (tag.length > 0) {
             console.log(`<--- ${title} <= ${tag.join(' <= ')}\n`
                 , ...content)
@@ -32,19 +39,26 @@ const csHandler = typeof window === 'object'
         }
     }
 
-const loggen = ({ level = ['info', 'debug', 'error', 'log'], handler }) => {
+const loggen = ({ level = ['trace', 'debug', 'info', 'warn', 'error', 'fatal'], handler }) => {
     function logger() { }
-    let genMethod = (level, handler) => (tag, ...content) => handler(level, tag, content)
+    let genMethod = (ls, level, handler) => (tag, ...content) => handler(ls, level, tag, content)
     for (let l of level) {
-        logger.prototype[l] = genMethod(l, handler)
+        logger.prototype[l] = genMethod(level, l, handler)
     }
     return new logger()
 }
 
-const conslog = title => loggen({ handler: csHandler(title) })
-const filelog = (fs, path) => loggen({ handler: fsHandler({fs, path})})
+const conslog = title => loggen({ handler: csHandler(title, defaultLvProbe()) })
+const filelog = (fs, path) => loggen({ handler: fsHandler({fs, path, lvProbe: defaultLvProbe('info')})})
 
 module.exports = {
     loggen, fsHandler, csHandler, conslog, filelog
 }
 
+/*
+let fs = require('fs')
+l1 = filelog(fs, './log')
+
+l1.debug([], 123)
+l1.info(['a','b','c'], 23)
+*/
